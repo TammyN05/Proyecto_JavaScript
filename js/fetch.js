91917f448e75b28fd5811542adb46d7f6ac2707c
TammyN05 <tammy.html@gmail.com>
2022-05-30 00:54:23 -0300

    master

--- js/fetch.js
+++ js/fetch.js
@@ -9,41 +9,41 @@
                     .then(res => res.json())
                     .then(data => {
                         console.log(data);
-                        mostrarEventos(data);  
+                        mostrarEvento(data);  
                 });
             };
 
             //LAS CARTAS ANASHI
 
             traerApi();
-            function mostrarEventos(data){
+            function mostrarEvento(data){
                 contenido =`
 
                 <div class="espectaculos py-5 ">
                     <div class="container">
                         <div class="row">`;
 
-                for(let eventos of data){
+                for(let evento of data){
                     contenido +=
-
-                        `<div class="col col-md-4 col-lg-3 py-1">
-                                <div class="card" style="width: 280px;">
-                                <img src="${eventos.imagen}" class="imagen">
-                                        <div class="card-img-overlay">
-                                            <h5 class="card-title">${eventos.nombre}</h5>
-                                            <p class="card-text"> Fecha: ${eventos.fecha}</p>
-                                            <p class="card-text1"> Tipo: ${eventos.tipo}</p>
-                                            <a href="#" class="btn">${eventos.comprar}</a>
-                                        </div>
-                                </div>
-                        </div>`
-
+               
+                    `<div class="col col-md-4 col-lg-3 py-1">
+                    <div class="card" style="width: 280px;">
+                    <img src="${evento.imagen}" class="imagen">
+                            <div class="card-img-overlay">
+                                <h5 class="card-title">${evento.nombre}</h5>
+                                <p class="card-text"> Fecha: ${evento.fecha}</p>
+                                <p class="card-text1"> Tipo: ${evento.tipo}</p>
+                                <a href=${evento.compra_link}  class="btn " target="_blank"> Comprar </a>
+                            </div>
+                    </div>
+            </div>`
+                 
                 };
                 contenido += 
                 `       </div>
                     </div>
                 </div>` 
-                 document.getElementById("eventos").innerHTML = contenido;             
+                 document.getElementById("evento").innerHTML = contenido;             
             }
 
     
